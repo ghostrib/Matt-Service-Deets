4c6ce70d2274888c12ddaa03ec94929663f14d07
gizmo <gizmo@Gremlin.local>
2019-01-31 11:45:57 -0600

    merging...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-18-216-54-110.us-east-2.compute.amazonaws.com',
+            host: 'ec2-18-191-0-101.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/related.pem',
             ref: 'origin/master',
             repo: 'git@github.com:ghostrib/Matt-Service-Related.git',
